783ab5dbd4dad18a8a57ecf8cbc5cb899b83ce7d
jason-napolitano <jnapolitanoit@gmail.com>
2026-09-18 06:34:14 -0700

    Model master.vue

--- resources/vue/layouts/master.vue
+++ resources/vue/layouts/master.vue
@@ -1,17 +1,11 @@
 <template>
     <Head :title="props.title" />
     <slot />
-
-    <div v-show="false" class="container mx-auto flex justify-center pt-2">
-        <DarkMode />
-    </div>
 </template>
 
 <script setup lang="ts">
 // --------------------------------------------------------
 // props
-import DarkMode from '@/components/buttons/dark-mode.vue'
-
 interface AppLayoutProps {
     title?: string | null
 }
