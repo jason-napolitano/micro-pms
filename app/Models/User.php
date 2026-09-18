0a03e41bd3c633883be355575b1fc28de9297c73
jason-napolitano <jnapolitanoit@gmail.com>
2026-09-17 20:16:07 -0700

    Update User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,19 +2,14 @@
 
 namespace App\Models {
 
-    use Illuminate\Database\Eloquent\Attributes\ObservedBy;
     use Illuminate\Foundation\Auth\User as Authenticatable;
-    use Illuminate\Database\Eloquent\Factories\HasFactory;
-    use Illuminate\Database\Eloquent\Concerns\HasUuids;
-    use Illuminate\Database\Eloquent\Casts\Attribute;
-    use Illuminate\Database\Eloquent\SoftDeletes;
-    use Illuminate\Database\Eloquent\Relations;
     use Illuminate\Notifications\Notifiable;
     use Spatie\Permission\Traits\HasRoles;
-    use App\Observers\UserObserver;
+    use Illuminate\Database\Eloquent;
     use Carbon\Carbon;
 
-    #[ObservedBy(UserObserver::class)]
+    #[Eloquent\Attributes\Fillable([['name', 'email', 'username', 'password', 'avatar']])]
+    #[Eloquent\Attributes\Hidden(['password', 'remember_token'])]
     /**
      * @method static where(string $where, string $is, ?string $operator = null)
      * @method static create(array $data)
@@ -23,26 +18,11 @@ namespace App\Models {
      */
     class User extends Authenticatable
     {
-        use SoftDeletes;
+        use Eloquent\Factories\HasFactory;
+        use Eloquent\Concerns\HasUuids;
+        use Eloquent\SoftDeletes;
         use Notifiable;
-        use HasFactory;
         use HasRoles;
-        use HasUuids;
-
-        /** @inheritdoc */
-        protected $fillable = [
-            'name',
-            'email',
-            'username',
-            'password',
-            'avatar',
-        ];
-
-        /** @inheritdoc */
-        protected $hidden = [
-            'password',
-            'remember_token',
-        ];
 
         // ------------------------------------------------
         // casts
@@ -59,12 +39,12 @@ namespace App\Models {
         // ------------------------------------------------
         // relations
 
-        public function properties(): Relations\BelongsToMany
+        public function properties(): Eloquent\Relations\BelongsToMany
         {
             return $this->belongsToMany(Property::class)->withTimestamps();
         }
 
-        public function assignedMakeReadyItems(): Relations\HasMany
+        public function assignedMakeReadyItems(): Eloquent\Relations\HasMany
         {
             return $this->hasMany(MakeReadyItem::class, 'assigned_to');
         }
@@ -72,16 +52,16 @@ namespace App\Models {
         // ------------------------------------------------
         // attributes
 
-        protected function createdAt(): Attribute
+        protected function createdAt(): Eloquent\Casts\Attribute
         {
-            return Attribute::make(
+            return Eloquent\Casts\Attribute::make(
                 get: static fn ($value) => Carbon::parse($value)->format('M d, Y'),
             );
         }
 
-        protected function updatedAt(): Attribute
+        protected function updatedAt(): Eloquent\Casts\Attribute
         {
-            return Attribute::make(
+            return Eloquent\Casts\Attribute::make(
                 get: static fn ($value) => Carbon::parse($value)->format('M d, Y'),
             );
         }
