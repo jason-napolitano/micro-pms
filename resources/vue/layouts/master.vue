d9615beb588d2c3b9c4837573af4d89a249f335c
jason-napolitano <jnapolitanoit@gmail.com>
2026-09-18 06:37:51 -0700

    Model master.vue

--- resources/vue/layouts/master.vue
+++ resources/vue/layouts/master.vue
@@ -1,11 +1,17 @@
 <template>
     <Head :title="props.title" />
     <slot />
+
+    <div v-show="false">
+        <DarkMode />
+    </div>
 </template>
 
 <script setup lang="ts">
 // --------------------------------------------------------
 // props
+import DarkMode from '@/components/buttons/dark-mode.vue'
+
 interface AppLayoutProps {
     title?: string | null
 }
